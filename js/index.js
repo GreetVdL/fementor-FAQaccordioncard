00fb40357daad6b1180cc615c4b29b329a221c84
Greet <vanderlindengreet@gmail.com>
2021-04-14 09:28:49 +0200

    js working now

--- js/index.js
+++ js/index.js
@@ -1,26 +1,25 @@
 // function to show and hide answers when questions are clicked
 
-function showAnswer() {}
-
-// function showAnswer() {
-//   const questions = document.querySelectorAll(".question");
-//   questions.forEach((question) => {
-//     question.addEventListener("click", () => {
-//       questions.forEach((question) => {
-//         question.classList.remove("active");
-//         question.nextElementSibling.style.display = "none";
-//       });
-//       question.classList.toggle("active");
-//       const activated = document.querySelector(".active");
-//       const answer = activated.nextElementSibling;
-//       answer.style.display = "block";
-//       activated.addEventListener("click", () => {
-//         activated.classList.remove("active");
-//         answer.style.display = "none";
-//       });
-//     });
-//   });
-// }
+function showAnswer() {
+  const questions = document.querySelectorAll(".question");
+  questions.forEach((question) => {
+    question.addEventListener("click", () => {
+      const activated = document.querySelector(".active");
+      let isActive = false;
+      if (question === activated) {
+        isActive = true;
+      }
+      questions.forEach((question) => {
+        question.classList.remove("active");
+        question.nextElementSibling.style.display = "none";
+      });
+      if (!isActive) {
+        question.classList.add("active");
+        question.nextElementSibling.style.display = "block";
+      }
+    });
+  });
+}
 
 // call showAnswer function when DOM has loaded
 

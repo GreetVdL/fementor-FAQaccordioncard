8f846a9137caad8c56955d62a5112cfd8e075903
Greet <vanderlindengreet@gmail.com>
2021-04-14 08:58:52 +0200

    wrong js functionality

--- js/index.js
+++ js/index.js
@@ -1,19 +1,26 @@
 // function to show and hide answers when questions are clicked
 
-function showAnswer() {
-  const questions = document.querySelectorAll(".question");
-  questions.forEach((question) => {
-    question.addEventListener("click", () => {
-      question.classList.toggle("active");
-      const activated = document.querySelector(".active");
-      if (activated !== null) {
-        const answer = activated.nextElementSibling;
-        answer.style.display = "block";
-        console.log(activated);
-      }
-    });
-  });
-}
+function showAnswer() {}
+
+// function showAnswer() {
+//   const questions = document.querySelectorAll(".question");
+//   questions.forEach((question) => {
+//     question.addEventListener("click", () => {
+//       questions.forEach((question) => {
+//         question.classList.remove("active");
+//         question.nextElementSibling.style.display = "none";
+//       });
+//       question.classList.toggle("active");
+//       const activated = document.querySelector(".active");
+//       const answer = activated.nextElementSibling;
+//       answer.style.display = "block";
+//       activated.addEventListener("click", () => {
+//         activated.classList.remove("active");
+//         answer.style.display = "none";
+//       });
+//     });
+//   });
+// }
 
 // call showAnswer function when DOM has loaded
 
